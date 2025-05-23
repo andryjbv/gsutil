2fdfac07536ed437f4ea4c8bd886cfc797ef98ba
Andry Becerra <andryjbv@gmail.com>
2025-05-23 00:15:54 -0400

    Implement run script and parser

--- run.sh
+++ run.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 ### COMMON SETUP; DO NOT MODIFY ###
 set -e
+cd /workspace
 
 # --- CONFIGURE THIS SECTION ---
 # Replace this with your command to run all tests
 run_all_tests() {
   echo "Running all tests..."
-  python -m unittest discover -s /app/gslib/tests -p 'test_*.py' -v
+  set +e
+  PYTHONPATH=app python -m unittest discover -s app/gslib/tests -t app -p 'test_*.py' -v
+  TEST_EXIT_CODE=$?
+  set -e
+  echo "Test run exited with code ${TEST_EXIT_CODE}"
 }
 
 # Replace this with your command to run specific test files
 run_selected_tests() {
   local test_files=("$@")
   echo "Running selected tests: ${test_files[@]}"
-  python -m unittest -v "${test_files[@]}"
+  set +e
+  PYTHONPATH=app python -m unittest -v "${test_files[@]}"
+  TEST_EXIT_CODE=$?
+  set -e
+  echo "Test run exited with code ${TEST_EXIT_CODE}"
 }
 # --- END CONFIGURATION SECTION ---
 
@@ -22,8 +31,8 @@ run_selected_tests() {
 
 # No args is all tests
 if [ $# -eq 0 ]; then
-  run_all_tests
-  exit $?
+run_all_tests
+exit 0
 fi
 
 # Handle comma-separated input
@@ -35,3 +44,4 @@ fi
 
 # Run them all together
 run_selected_tests "${TEST_FILES[@]}"
+exit 0
